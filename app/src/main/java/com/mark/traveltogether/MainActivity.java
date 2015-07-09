2341f9210b55bb3795ccb5908e83f2dae44c1dd4
Mark <love123zst@163.com>
2015-07-09 18:11:03 +0800

    baidu map added

--- app/src/main/java/com/mark/traveltogether/MainActivity.java
+++ app/src/main/java/com/mark/traveltogether/MainActivity.java
@@ -5,19 +5,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import java.util.Map;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    MapView mMapView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        mMapView = (MapView)findViewById(R.id.bmapView);
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
